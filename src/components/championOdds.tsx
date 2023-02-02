c883318452886cd9a90bc7137173ebaeeeaada45
Yannick Ferire <yannick@bureau347.com>
2023-02-02 22:19:03 +0100

    Improve style for champion portrait

--- src/components/championOdds.tsx
+++ src/components/championOdds.tsx
@@ -22,16 +22,17 @@ const ChampionOdds: React.FC<IChampionOdds> = ({ champion, selectedLevel, pool, 
 
   return (
     <>
-      <img 
-        className={`w-24 h-full mr-4 border-4 border-${champion.cost}cost rounded`}
-        src={`${championImageURL}/${champion.apiName.toLowerCase()}_square.tft_set${currentSet}.png`} 
-        alt={champion.name} />
-      <div className="mr-4 flex flex-1 flex-wrap">
-        <div className="flex">
-          <h3 className="text-lg">{champion.name}</h3>
-          <span className="mx-1">–</span>
-          <p><GoldIcon color="midnight" /> {champion.cost}</p>
+      <header className="flex flex-col mr-4">
+        <div className="relative">
+          <img 
+            className={`w-24 mb-1 h-full border-4 border-${champion.cost}cost rounded`}
+            src={`${championImageURL}/${champion.apiName.toLowerCase()}_square.tft_set${currentSet}.png`} 
+            alt={champion.name} />
+          <p className={`absolute text-crema text-sm px-2 pb-0.5 text-center rounded bottom-0 bg-${champion.cost}cost`}> <GoldIcon color="crema" size={2.5} /> {champion.cost}</p>
         </div>
+        <h3 className="mt-1 text-center font-medium">{champion.name}</h3>
+      </header>
+      <div className="mr-4 flex flex-1 flex-wrap">
         <CopiesNumber ownedCopies={ownedCopies} setOwnedCopies={setOwnedCopies} />
       </div>
       <OddsByStar star={1} champion={champion} selectedLevel={selectedLevel} pool={pool} setPool={setPool} ownedCopies={ownedCopies} />
