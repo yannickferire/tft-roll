d6894b5d52a66466464f47a6bea1001b0e573f1c
Yannick Ferire <yannick@bureau347.com>
2023-03-13 14:20:01 +0100

    Add grayscale on remove hover effect

--- src/components/championOdds.tsx
+++ src/components/championOdds.tsx
@@ -43,23 +43,23 @@ const ChampionOdds: React.FC<IChampionOdds> = ({ champion, selectedLevel, pool, 
       <header className="flex flex-col mr-4">
         <div 
           onClick={() => handleRemoveChampion(champion.name)}
-          className="relative bg-midnight rounded group cursor-pointer">
+          className="w-24 relative bg-midnight rounded group cursor-pointer">
             <img 
-              className={`w-24 mb-1 h-full border-4 border-${champion.cost}cost rounded group-hover:opacity-25 transition duration-500`}
+              className={`mb-1 h-full border-4 border-${champion.cost}cost rounded group-hover:grayscale group-hover:opacity-25 transition duration-500`}
               src={champion.image} 
               alt={champion.name} />
-              <span className={`group-hover:scale-100 group-hover:opacity-100 transition duration-500 scale-75 origin-center opacity-0 absolute -mt-1 ml-1 text-6xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rotate-45 text-${champion.cost}cost`}>+</span>
-          <p className={`absolute text-crema text-sm px-2 pb-0.5 text-center rounded bottom-0 bg-${champion.cost}cost group-hover:opacity-25 transition duration-500`}> <GoldIcon color="crema" size={2.5} /> {champion.cost}</p>
+            <span className={`group-hover:scale-100 group-hover:opacity-100 transition duration-500 scale-75 origin-center opacity-0 absolute -mt-1 ml-1 text-6xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rotate-45 text-${champion.cost}cost`}>+</span>
+          <p className={`absolute text-crema text-sm px-2 pb-0.5 text-center rounded bottom-0 bg-${champion.cost}cost group-hover:grayscale group-hover:opacity-25 transition duration-500`}> <GoldIcon color="crema" size={2.5} /> {champion.cost}</p>
         </div>
-        <h3 className="mt-1 mb-1 text-center font-medium">{champion.name}</h3>
-        <ul className="flex justify-center gap-1">
+        <h3 className="w-24 mt-2 mb-2 text-center font-medium leading-4">{champion.name}</h3>
+        <ul className="w-24 flex justify-center gap-1">
           {champion.traits.map((trait, index) => {
             const traitImage = traits.find(traitObj => traitObj.name === trait).image;
             const path = trait === 'Threat' ? 'triangle pt-2 px-[6px] w-7' : 'hex w-6';
 
             return (
               <li className={`${path} h-7 flex items-center justify-center p-1 text-xs bg-midnight opacity-50`} key={index}>
-                <img src={traitImage} alt={trait} />
+                <img src={traitImage} alt={trait} title={trait} />
               </li>
             )
           })}
